858d61d6558d0f7e0675f8cf52d3b471be8354ab
haris155 <harisrehman155@gmail.com>
2022-09-30 01:05:41 +0500

    Assignment2

--- A1.js
+++ A1.js
@@ -1,6 +1,34 @@
-var name = prompt('Enter Your Name ');
-var msg = `Hello ${name}, Would you like to learn some python`;
-var txt_name = document.getElementById('myname');
-var text = document.createTextNode(" !!!")
-txt_name.innerHTML = msg;
-txt_name.appendChild(text);
+function CamelCase(name) {
+    var temp = '';
+    var tempList = [];
+    var list = name.split(' ');
+    var fname = list[0].charCodeAt(0);
+    var lname = list[1].charCodeAt(0);
+
+    if (fname >= 97) {
+        var diff = fname - 97;
+        fname = 65 + diff;
+        temp = String.fromCharCode(fname) + list[0].substr(1);
+        tempList.push(temp);
+    }
+    if (lname >= 97) {
+        var diff = lname - 97;
+        lname = 65 + diff;
+        temp = String.fromCharCode(lname) + list[1].substr(1);
+        tempList.push(temp);
+    }
+
+    return tempList;
+}
+var name = prompt("Enter Your Name");
+document.getElementById("mynameup").innerHTML = name.toUpperCase();
+document.getElementById("mynamelow").innerHTML = name.toLowerCase();
+var newlist = CamelCase(name);
+console.log(newlist);
+var word = '';
+for (let i = 0; i < newlist.length; i++) {
+    word += newlist[i] + " ";
+    console.log(word);
+
+}
+document.getElementById("mynamecc").innerHTML = word;
