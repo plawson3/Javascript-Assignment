73a707ac37635c75b5de534e06b53dc17d202f27
haris155 <harisrehman155@gmail.com>
2022-09-30 23:40:36 +0500

    Assignment9

--- A1.js
+++ A1.js
@@ -1,3 +1,6 @@
+//Taking Number from User 
 var no = prompt('Enter Your Favourite Number: ');
+//Creating Message to Display
 var msg = `Is ${no}, Your Favourite Number?`;
+// Injecting Message into Html of Id msg 
 document.getElementById('msg').innerHTML = msg;
