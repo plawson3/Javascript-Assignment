3fe763e4510371a5bc366a8fbaffa563804d0293
haris155 <harisrehman155@gmail.com>
2022-10-01 02:20:30 +0500

    Assignment14

--- A1.js
+++ A1.js
@@ -6,3 +6,8 @@ for (let i = 0; i < list.length; i++) {
     p.textContent = "I Would Like to Invite " + list[i].toUpperCase() + " on Dinner!!";
     statement.append(p);
 }
+
+var newGuest = "asif";
+let p = document.createElement('p');
+p.textContent = "I Would Like to Invite " + newGuest.toUpperCase() + " on Dinner. Because one of my guest is unable to join us !!";
+statement.append(p);
