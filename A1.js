d9bc387fbd38718a623bca64ff7b66e16bc8e6e1
haris155 <harisrehman155@gmail.com>
2022-09-30 22:29:42 +0500

    Assignment3

--- A1.js
+++ A1.js
@@ -1,34 +1,4 @@
-function CamelCase(name) {
-    var temp = '';
-    var tempList = [];
-    var list = name.split(' ');
-    var fname = list[0].charCodeAt(0);
-    var lname = list[1].charCodeAt(0);
-
-    if (fname >= 97) {
-        var diff = fname - 97;
-        fname = 65 + diff;
-        temp = String.fromCharCode(fname) + list[0].substr(1);
-        tempList.push(temp);
-    }
-    if (lname >= 97) {
-        var diff = lname - 97;
-        lname = 65 + diff;
-        temp = String.fromCharCode(lname) + list[1].substr(1);
-        tempList.push(temp);
-    }
-
-    return tempList;
-}
-var name = prompt("Enter Your Name");
-document.getElementById("mynameup").innerHTML = name.toUpperCase();
-document.getElementById("mynamelow").innerHTML = name.toLowerCase();
-var newlist = CamelCase(name);
-console.log(newlist);
-var word = '';
-for (let i = 0; i < newlist.length; i++) {
-    word += newlist[i] + " ";
-    console.log(word);
-
-}
-document.getElementById("mynamecc").innerHTML = word;
+var name = prompt("Name Any Famous Person!");
+var quote = prompt("Enter Qoute of that Person....");
+var msg = `${name} once said, "${quote}"`;
+document.getElementById("quote").innerHTML = msg;
