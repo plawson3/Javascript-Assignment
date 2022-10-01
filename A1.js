6efdb71df40dce7ca3cb475f3597e72a77a31812
haris155 <harisrehman155@gmail.com>
2022-10-01 11:48:15 +0500

    Assignment17 Shrinking Guest List

--- A1.js
+++ A1.js
@@ -6,28 +6,36 @@ function bind(ele, list) {
     }
 }
 
-var list = ['Ammad'];
+var list = ['Ammad', "Ali", 'Amir', 'Alishan'];
+var popList = []
 var statement = document.querySelector('#statement');
 bind(statement, list);
 
-//printing guest at the beginning
-var stat1 = document.querySelector('#stat1');
-var guestAtBeginning = "Ali";
-list.unshift(guestAtBeginning);
-bind(stat1, list);
+for (let i = 0; i < list.length; i++) {
 
-//printing guest at the Middle
-var len = parseInt(list.length / 2);
-var stat2 = document.querySelector('#stat2');
-var guestInMiddle = "Amir";
-list.splice(len, 0, guestInMiddle);
-bind(stat2, list);
-
-
-//printing guest at the End
-var stat3 = document.querySelector('#stat3');
-var guestAtBeginning = "Alishan";
-list.push(guestAtBeginning);
-bind(stat3, list);
-
+    let p = document.createElement('p');
+    var unInvited = document.querySelector('#un-invited');
+    var Invited = document.querySelector('#invited');
+    if (i < 2) {
+        p.textContent = list[i] + " sorry you aren't invited to dinner.";
+        unInvited.append(p);
+        //getting index of element which is meant to be removed
+        var idx = list.indexOf(list[i]);
+        //inserting index of elements, so can pop in the end
+        popList.pop(idx);
+        //function to remove 1 element at a time using index of that element.
+        // list.splice(idx, 1);
+    }
+    else {
+        p.textContent = list[i] + " You're invited to dinner.";
+        Invited.append(p);
+        var idx = list.indexOf(list[i]);
+        popList.pop(idx);
+        // list.splice(idx, 1);
+    }
+}
+//Remove every item from the list which is referenced into the pop List.
+list.splice(popList, popList.length);
+var emptyList = document.querySelector('#stat3');
+emptyList.append('List is Empty');
 
