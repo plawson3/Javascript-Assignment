a4b9e892c05509c819fe7788ee581c51e5c66477
haris155 <harisrehman155@gmail.com>
2022-10-01 11:10:16 +0500

    Assignment16

--- A1.js
+++ A1.js
@@ -6,24 +6,28 @@ function bind(ele, list) {
     }
 }
 
-var list = ['Ali', 'Alam', 'Ammad', 'Asir'];
+var list = ['Ammad'];
 var statement = document.querySelector('#statement');
 bind(statement, list);
 
-//printing not comming guest naem
-var notCommingGuest = list[3] + " is not comming!!";
-document.getElementById('stat1').innerHTML = notCommingGuest;
+//printing guest at the beginning
+var stat1 = document.querySelector('#stat1');
+var guestAtBeginning = "Ali";
+list.unshift(guestAtBeginning);
+bind(stat1, list);
 
-//removing not comming guest form the list
-list.pop(notCommingGuest);
-
-//adding new guest to the list
-var newGuest = "asif";
-list.push(newGuest);
-var statement1 = document.querySelector('#stat2');
-bind(statement1, list);
+//printing guest at the Middle
+var len = parseInt(list.length / 2);
+var stat2 = document.querySelector('#stat2');
+var guestInMiddle = "Amir";
+list.splice(len, 0, guestInMiddle);
+bind(stat2, list);
 
 
-let p = document.createElement('p');
-p.textContent = "I Would Like to Invite " + newGuest.toUpperCase() + " on Dinner. Because one of my guest is unable to join us !!";
-statement.append(p);
+//printing guest at the End
+var stat3 = document.querySelector('#stat3');
+var guestAtBeginning = "Alishan";
+list.push(guestAtBeginning);
+bind(stat3, list);
+
+
