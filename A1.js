2625fb2b7e717918e9e6700d87afcdfc252b046d
haris155 <harisrehman155@gmail.com>
2022-10-07 00:11:30 +0500

    Assignment 26 Alien Colors #2

--- A1.js
+++ A1.js
@@ -3,8 +3,8 @@ let alien_color = 'yellow';
 if (alien_color == 'green') {
     console.log(' player just earned 5 points.');
 }
-else if (alien_color == 'yellow') {
-    // console.log(' player just earned 5 points.');
+else if (alien_color != 'green') {
+    console.log(' player just earned 10 points.');
 }
 else if (alien_color == 'red') {
     // console.log(' player just earned 5 points.');
