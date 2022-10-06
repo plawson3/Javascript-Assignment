a4babeabf421e43c75650f579345a9f5594a453d
haris155 <harisrehman155@gmail.com>
2022-10-06 22:32:14 +0500

    Assignment 21 Think of something you could store in a array

--- A1.js
+++ A1.js
@@ -1,27 +1,7 @@
-var list = ['canda', 'europe', 'swizerland', 'paris', 'australia'];
-document.getElementById('list1').innerHTML = list;
-
-var sortedList = [...list];
-document.getElementById('list2').innerHTML = sortedList.sort(function (a, b) {
-    return a.localeCompare(b);
-});
-
-document.getElementById('list3').innerHTML = list;
-
-var reversesortedList = [...list];
-document.getElementById('list4').innerHTML = reversesortedList.sort(function (a, b) {
-    return b.localeCompare(a);
-});
-
-document.getElementById('list5').innerHTML = list;
-
-var reverseList = [...list];
-document.getElementById('list6').innerHTML = reverseList.reverse();
-
-document.getElementById('list7').innerHTML = list;
-
-document.getElementById('list8').innerHTML = list.sort(function (a, b) {
-    return a.localeCompare(b);
-});
-
-document.getElementById('list9').innerHTML = list.reverse();
+const list = [
+    ['k2', 'mount everst', 'himalya'],
+    ['river1', 'river2', 'river3',],
+    ['pakistan', 'india', 'china',],
+    ['karachi', 'lahore', 'multan',],
+    ['englist', 'urdu', 'arabic',],
+]
