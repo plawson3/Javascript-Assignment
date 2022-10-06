6fa0e377c38891f511030c528746f9ff78cd0088
haris155 <harisrehman155@gmail.com>
2022-10-07 00:12:38 +0500

    Assignment 27 Alien Colors #3

--- A1.js
+++ A1.js
@@ -1,11 +1,11 @@
-let alien_color = 'yellow';
+let alien_color = 'red';
 
 if (alien_color == 'green') {
     console.log(' player just earned 5 points.');
 }
-else if (alien_color != 'green') {
+else if (alien_color == 'yellow') {
     console.log(' player just earned 10 points.');
 }
 else if (alien_color == 'red') {
-    // console.log(' player just earned 5 points.');
+    console.log(' player just earned 15 points.');
 }
