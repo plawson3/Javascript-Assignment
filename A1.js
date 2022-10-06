8cb249abfeec2a2f17f17fc68819e8ca67b70cc2
haris155 <harisrehman155@gmail.com>
2022-10-06 23:43:59 +0500

    Assignment 23 Conditional Tests

--- A1.js
+++ A1.js
@@ -1,4 +1,33 @@
-const list = ['k2', 'mount everst', 'himalya']
-document.getElementById('list').innerHTML = list;
-document.getElementById('list1').innerHTML = list[5];
-document.getElementById('list2').innerHTML = list[2];
+let car = "ferrari";
+if (car == "ferrari") {
+    console.log("you predicted True");
+}
+else {
+    console.log("you predicted False");
+}
+
+let grade = "";
+let marks = Number(prompt("Enter Your marks"));
+
+console.log(marks, typeof marks);
+
+if (marks > 0 && marks < 60) {
+    grade = "Better luck next Time, F";
+}
+else if (marks >= 60 && marks <= 70) {
+    grade = "Your Grade is C";
+}
+else if (marks > 70 && marks <= 80) {
+    grade = "Your Grade is B";
+}
+else if (marks > 80 && marks <= 90) {
+    grade = "Your Grade is A";
+}
+else if (marks > 90 && marks <= 100) {
+    grade = "Your Grade is A+";
+}
+else if (marks < 0 || marks > 100) {
+    grade = "Wrong Marks";
+}
+
+document.getElementById('marks').innerHTML = grade;
