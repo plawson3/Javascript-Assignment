28fd2e2d6e8204239a1db146ed1b613c5cd583ca
haris155 <harisrehman155@gmail.com>
2022-10-07 00:09:51 +0500

    Assignment 25 Alien Colors #1

--- A1.js
+++ A1.js
@@ -1,62 +1,11 @@
-let car = "ferrari";
-if (car == "ferrari") {
-    console.log("you predicted True");
-}
-else {
-    console.log("you predicted False");
-}
+let alien_color = 'yellow';
 
-let grade = "";
-let marks = 0;//Number(prompt("Enter Your marks"));
-
-
-if (marks >= 0 && marks < 60) {
-    grade = "Better luck next Time, F";
+if (alien_color == 'green') {
+    console.log(' player just earned 5 points.');
 }
-else if (marks >= 60 && marks <= 70) {
-    grade = "Your Grade is C";
+else if (alien_color == 'yellow') {
+    // console.log(' player just earned 5 points.');
 }
-else if (marks > 70 && marks <= 80) {
-    grade = "Your Grade is B";
+else if (alien_color == 'red') {
+    // console.log(' player just earned 5 points.');
 }
-else if (marks > 80 && marks <= 90) {
-    grade = "Your Grade is A";
-}
-else if (marks > 90 && marks <= 100) {
-    grade = "Your Grade is A+";
-}
-else if (marks < 0 || marks > 100) {
-    grade = "Wrong Marks";
-}
-console.log(grade);
-//document.getElementById('marks').innerHTML = grade;
-
-//vale and type checking
-let mystring = "123";
-if (mystring === 123) {
-    console.log("True");
-}
-else {
-    console.log("False");
-}
-
-//upper and lower case checking 
-let mystring1 = "haris";
-if (mystring1.toUpperCase() === 'haris'.toLowerCase()) {
-    console.log(mystring1, "True");
-}
-else {
-    console.log(mystring1, "False");
-}
-
-const list = ['haris', 'ali', 'ammad', 'azhar', 'ashar'];
-if (list.includes('rehman')) {
-    console.log("rehman is located at index ", list.indexOf('rehman'), "True");
-}
-else if (list.includes("haris")) {
-    console.log("haris is in list at index ", list.indexOf('haris'));
-}
-else {
-    console.log("No Element Found");
-}
-
