30233516fba01e01d4d34821c60e12bf8bed0cc0
haris155 <harisrehman155@gmail.com>
2022-10-06 22:13:55 +0500

    Assignment 18 Seeing the World

--- A1.js
+++ A1.js
@@ -1,12 +1,27 @@
-var dict = new Object();
+var list = ['canda', 'europe', 'swizerland', 'paris', 'australia'];
+document.getElementById('list1').innerHTML = list;
 
-dict['mountains'] = ['mount everst', 'k2', 'himalaya'];
-dict['rivers'] = ['river A', 'river B', 'river C'];
-dict['countries'] = ['pakistan', 'india', 'china'];
-dict['cities'] = ['karachi', 'isalamabad', 'lahore'];
-dict['languages'] = ['english', 'urdu', 'arabic'];
+var sortedList = [...list];
+document.getElementById('list2').innerHTML = sortedList.sort(function (a, b) {
+    return a.localeCompare(b);
+});
 
+document.getElementById('list3').innerHTML = list;
 
-// for (var key in dict) {
-//     console.log(dict[key], key);
-// }
+var reversesortedList = [...list];
+document.getElementById('list4').innerHTML = reversesortedList.sort(function (a, b) {
+    return b.localeCompare(a);
+});
+
+document.getElementById('list5').innerHTML = list;
+
+var reverseList = [...list];
+document.getElementById('list6').innerHTML = reverseList.reverse();
+
+document.getElementById('list7').innerHTML = list;
+
+document.getElementById('list8').innerHTML = list.sort(function (a, b) {
+    return a.localeCompare(b);
+});
+
+document.getElementById('list9').innerHTML = list.reverse();
