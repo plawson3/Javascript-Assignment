f3eff44f7aa5f88e29745ebd9a4c9b14316d16da
haris155 <harisrehman155@gmail.com>
2022-10-07 00:41:29 +0500

    Assignment 28 Stages of Life

--- A1.js
+++ A1.js
@@ -1,11 +1,20 @@
-let alien_color = 'red';
+let age = 1;
 
-if (alien_color == 'green') {
-    console.log(' player just earned 5 points.');
+if (age < 2) {
+    console.log('baby');
 }
-else if (alien_color == 'yellow') {
-    console.log(' player just earned 10 points.');
+else if (age >= 2 && age < 4) {
+    console.log(' toddler');
 }
-else if (alien_color == 'red') {
-    console.log(' player just earned 15 points.');
+else if (age >= 4 && age < 13) {
+    console.log('kid');
+}
+else if (age >= 13 && age < 20) {
+    console.log('teenager');
+}
+else if (age >= 20 && age < 65) {
+    console.log('adult');
+}
+else if (age >= 65) {
+    console.log('elder');
 }
