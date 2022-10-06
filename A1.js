58e87d5ca1c319ceb375a3c78398cb20b4b853d0
haris155 <harisrehman155@gmail.com>
2022-10-06 22:41:13 +0500

    Assignment 22 Intentional Erro

--- A1.js
+++ A1.js
@@ -1,7 +1,4 @@
-const list = [
-    ['k2', 'mount everst', 'himalya'],
-    ['river1', 'river2', 'river3',],
-    ['pakistan', 'india', 'china',],
-    ['karachi', 'lahore', 'multan',],
-    ['englist', 'urdu', 'arabic',],
-]
+const list = ['k2', 'mount everst', 'himalya']
+document.getElementById('list').innerHTML = list;
+document.getElementById('list1').innerHTML = list[5];
+document.getElementById('list2').innerHTML = list[2];
