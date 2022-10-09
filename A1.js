242245b8d730c0c9ee6c6a699b6c2112036faf35
haris155 <harisrehman155@gmail.com>
2022-10-10 00:55:58 +0500

    Assignment 42 Great Magicians

--- A1.js
+++ A1.js
@@ -1,14 +1,18 @@
 let msg = document.querySelector('#tshirt');
-function show_magicians(list) {
+function show_magicians(i) {
+    // console.log(item);
+    return list[i];
+}
+function make_great(list) {
 
-    for (const item in list) {
+    for (let i = 0; i < list.length; i++) {
 
         let p = document.createElement('p');
-        p.textContent = `${list[item]}`;
+        p.textContent = `The Great ${show_magicians(i)}`;
         msg.append(p);
     }
+
 }
 
-
 const list = ['AMASIS', 'THE AMAZING JOHNATHAN', 'CRISS ANGEL', 'THEODORE ANNEMANN'];
-show_magicians(list);
+make_great(list);
