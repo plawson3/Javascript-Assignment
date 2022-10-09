b4ad1b756658bac0ee28aa991df80048a319a53e
haris155 <harisrehman155@gmail.com>
2022-10-10 00:08:49 +0500

    Assignment 37 Large Shirts

--- A1.js
+++ A1.js
@@ -1,8 +1,9 @@
 let msg = document.querySelector('#tshirt');
-function make_shirt(size, text) {
+function make_shirt(size = 'large', text = 'I Love JavaScript') {
     let p = document.createElement('p');
     p.textContent = `The size of shirt is ${size} and Instruction is  ${text}`;
     msg.append(p);
 }
 
-make_shirt(12, 'Height should be same as given in inches');
+make_shirt("Medium", "I Love C#");
+make_shirt();
