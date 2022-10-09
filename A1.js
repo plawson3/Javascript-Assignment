984f2106ae059eb90946bf5bd5251fc0bdb441e0
haris155 <harisrehman155@gmail.com>
2022-10-10 01:21:23 +0500

    Assignment 44 Sandwiches

--- A1.js
+++ A1.js
@@ -1,24 +1,22 @@
 let arr1 = document.querySelector('#oldArr');
-let arr2 = document.querySelector('#newArr');
-function show_magicians(i) {
-    return list[i];
-}
-function make_great(list) {
+
+function items(list) {
 
     for (let i = 0; i < list.length; i++) {
-
         let p = document.createElement('p');
-        let p1 = document.createElement('p');
-
-        p.textContent = `${show_magicians(i)}`;
+        p.textContent = list[i];
         arr1.append(p);
-
-        p1.textContent = `The Great ${show_magicians(i)}`;
-        arr2.append(p1);
-
     }
-
 }
 
-const list = ['AMASIS', 'THE AMAZING JOHNATHAN', 'CRISS ANGEL', 'THEODORE ANNEMANN'];
-make_great(list);
+const list = [];
+while (true) {
+    let input = prompt('Enter Items you wants on sandwitch, Press Enter when done');
+    if (input != "") {
+        list.push(input);
+    }
+    else {
+        break;
+    }
+}
+items(list);
