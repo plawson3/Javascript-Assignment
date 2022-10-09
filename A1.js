db99a8315771ec06a0113e06569cb5512321c34d
haris155 <harisrehman155@gmail.com>
2022-10-10 00:32:15 +0500

    Assignment 40 Album

--- A1.js
+++ A1.js
@@ -1,11 +1,21 @@
 let msg = document.querySelector('#tshirt');
-function city_country(city = 'Karachi', county = 'Pakistan') {
-    let p = document.createElement('p');
-    p.textContent = ` ${city}, ${county}`;
-    msg.append(p);
+function city_country(name = 'Atif Aslam', title = 'Kuch is tarha') {
+
+    let dict = new Object();
+    dict["Name"] = name;
+    dict["Title"] = title;
+
+    for (const key in dict) {
+
+        let p = document.createElement('p');
+        p.textContent = ` ${key} : ${dict[key]}`;
+        msg.append(p);
+    }
+    msg.append(document.createElement('hr'));
 }
 
+
 city_country();
-city_country("London", "Englan");
-city_country("Sydney", "Australia");
-city_country("Delhi", "India");
+city_country("In the End", "Linkin Park");
+// city_country("Sydney", "Australia");
+// city_country("Delhi", "India");
