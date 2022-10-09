5d0882900fb5623888c220c16c86380fde9b9650
haris155 <harisrehman155@gmail.com>
2022-10-09 22:49:07 +0500

    Assignment 33 Ordinal Numbers:

--- A1.js
+++ A1.js
@@ -1,14 +1,15 @@
-const current_users = ['haris', 'ali', 'ammad', 'azhar', 'Ashar'];
-const new_users = ['amir', 'alishan', 'AMMAD', 'babar', 'ashar'];
+for (let i = 0; i < 10; i++) {
 
-for (let i = 0; i < current_users.length; i++) {
-    for (let j = 0; j < new_users.length; j++) {
-        if (current_users[i].toLowerCase().trim() != new_users[j].toLowerCase().trim()) {
-            console.log(`username is available ${current_users[i]}`);
-        }
-        else {
-            console.log(`enter a new username, this ${new_users[i]} username has already been taken!!!`);
-        }
-
+    if (i == 1) {
+        console.log(`${i}st`);
+    }
+    else if (i == 2) {
+        console.log(`${i}nd`);
+    }
+    else if (i == 3) {
+        console.log(`${i}rd`);
+    }
+    else {
+        console.log(`${i}th`);
     }
 }
