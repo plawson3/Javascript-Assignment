1b04a5c3fbd255d0bf189101c74cd4790863fcb3
haris155 <harisrehman155@gmail.com>
2022-10-09 23:47:56 +0500

    Assignment 35 Animals

--- A1.js
+++ A1.js
@@ -1,7 +1,20 @@
-let list = ['Cheese Overload', 'Cheese Galore', 'Mission Impizzable'];
+let list = ['Dog', 'Cat', 'Parrot'];
+
+var statement = document.querySelector('#Animals');
+var statement1 = document.querySelector('#aboutAnimals');
+list.map(function (a) {
+    let p = document.createElement('p');
+    p.textContent = ` ${a}`;
+    statement.append(p);
+})
 
 for (let i = 0; i < list.length; i++) {
-    console.log(`I like pepperoni ${list[i]} pizza!!`);
+    let input = prompt(`Enter a statment about ${list[i]}`);
+    let p = document.createElement('p');
+    p.textContent = ` ${list[i]}: ${input}`;
+    statement1.append(p);
 }
-console.log("I really love pizza!");
 
+let p1 = document.createElement('p');
+p1.textContent = "All Animals are Good!!!!!";
+statement1.append(p1);
